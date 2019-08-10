c1ff5990c3241be0283aeac93fe838a0febeddfe
joannayhs <joannayuhas@gmail.com>
2019-08-10 12:34:33 -0400

    updated user controller to redirect to user page

---
+++ app/controllers/donation_controller.rb
@@ -0,0 +1,6 @@
+class DonationController < ApplicationController 
+
+  
+
+
+end 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,26 +1,35 @@
 class UserController < ApplicationController
+    
+    get '/:username' do 
+        if logged_in?
+            @user = current_user
+            erb :'/user/show'
+        else 
+            redirect to '/login'
+        end 
+    end 
 
     get '/login' do 
      if !logged_in?
         erb :'/user/login'
      else 
-        redirect to '/#{current_user.username}/donations'
+        redirect to "/#{current_user.username}"
      end 
     end 
 
     post '/login' do 
        @user = User.find_by(username: params[:username])
        if @user && @user.authenticate(params[:password])
-        session[:id] = @user.id 
-        redirect to "/#{current_user.username}/donations"
+        session[:user_id] = @user.id 
+        redirect to "/#{current_user.username}"
        else 
         redirect to '/signup'
        end 
     end 
 
     get '/signup' do 
-     if logged_in? 
-        redirect to "/#{current_user.username}/donations"
+     if logged_in?
+        redirect to "/#{current_user.username}"
      else 
         erb :'/user/signup'
      end 
@@ -32,12 +41,22 @@ class UserController < ApplicationController
         else 
             if params[:username] != User.find_by(username: params[:username])
                 @user = User.create(params) 
-                session[:id] = @user.id 
+                session[:user_id] = @user.id 
+                redirect to "/#{current_user.username}"
             else 
                 redirect to '/signup'
             end 
         end 
     end 
     
+   get 'logout' do 
+        if logged_in?
+            session.destroy
+            redirect '/'
+        else 
+            redirect '/login'
+        end 
+    end 
+
     
 end 
