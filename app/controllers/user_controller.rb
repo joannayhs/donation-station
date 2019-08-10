0da1b930e789f0321c81756f4d5c1df48db3136c
joannayhs <joannayuhas@gmail.com>
2019-08-10 11:35:01 -0400

    built login paths for user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,21 @@
 class UserController < ApplicationController
 
+    get '/login' do 
+     if !logged_in?
+        erb :'/user/login'
+     else 
+        redirect to '/#{current_user.username}/donations'
+     end 
+    end 
+
+    post '/login' do 
+       @user = User.find_by(username: params[:username])
+       if @user && @user.authenticate[params[:password])
+        session[:id] = @user.id 
+        redirect to "/#{current_user.username}/donations"
+       else 
+        redirect to '/signup'
+       end 
+    end 
     
 end 
