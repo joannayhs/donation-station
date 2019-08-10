c78ce7b0e1027cf66e2c844083ccdcc49346eca1
joannayhs <joannayuhas@gmail.com>
2019-08-10 15:54:46 -0400

    created new donation post route

--- app/controllers/donation_controller.rb
+++ app/controllers/donation_controller.rb
@@ -11,11 +11,28 @@ class DonationController < ApplicationController
 
     get '/donations/new' do 
         if logged_in? 
+            @charities = Charity.all
             erb :'/donation/new'
         else 
             redirect to '/login'
         end 
     end 
 
+    post '/donations' do 
+        if logged_in? 
+            if params[:donations][:amount] != nil && params[:charities][:name] != "" && params[:charities][:description] != ""
+                @donation = Donation.create(amount: params[:donations][:amount])
+                @charity = Charity.create(name: params[:charities][:name], description: params[:charities][:description])
+                @charity.donations << @donation 
+                @user = current_user 
+                @user.donations << @donation
+                redirect to '/donations'
+            else 
+                redirect to '/donations/new'
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end 
 
 end 
